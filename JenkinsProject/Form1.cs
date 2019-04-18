0611125673efb5c5bee0760288357511abfb1d41
이원석 <maruchi84712@naver.com>
2019-04-18 13:55:20 +0900

    Fail2

--- JenkinsProject/Form1.cs
+++ JenkinsProject/Form1.cs
@@ -23,7 +23,7 @@ namespace JenkinsProject
             label1.Text = "Ten";
             label1.Text = "LastTest";
             label1.Text = "Fail"; 
-            label1.Text="Jira Fail Plz";
+            label1.Text="Jira Fail Plz"f2;
         }
     }
 }
